77e6e213ba2e449200fc70bb6f58d0f7e23adabf
Situn Roman <romansitun@yandex.ru>
2022-03-14 20:50:13 +0500

    после первого ревью

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 let popup = document.querySelector('.popup');
 let buttonShow = document.querySelector('.profile__button');
 let buttonHide = document.querySelector('.popup__close-button');
-
-
-buttonShow.addEventListener ('click', function(){
-    popup.classList.add('popup_opened');
-})
-
-
-buttonHide.addEventListener ('click', function(){
-    popup.classList.remove('popup_opened');
-})
-
-
 let formElement = document.querySelector('.popup__form'); 
-
 let nameInput = document.querySelector('.popup__form-item_type_name');
 let jobInput = document.querySelector('.popup__form-item_type_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__occupation');
+
+
+function openPopup(){
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+};
+
+function closePopup(){
+    popup.classList.remove('popup_opened');
+};
+
+
+buttonShow.addEventListener ('click', openPopup);
+
+
+
+buttonHide.addEventListener ('click', closePopup);
+
+
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    document.querySelector('.profile__name').textContent = nameInput.value;
-    document.querySelector('.profile__occupation').textContent = jobInput.value;
-
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
 }
 
+
 formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', closePopup); 
