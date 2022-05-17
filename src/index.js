e56f0a49ed1b2269b2adfcbeaf6ebef92f53bf5c
Situn Roman <romansitun@yandex.ru>
2022-05-17 21:53:06 +0500

    feat 8ПР до ревью

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import Card from "../components/Card.js";
-import FormValidator from "../components/FormValidator.js";
+import './pages/index.css';
+import Card from "./components/Card.js";
+import FormValidator from "./components/FormValidator.js";
 import {
   initialCards, profilePopup, buttonShowProfilePopup, profileFormElement,
   nameInput, jobInput, profileName, profileJob, buttonShowCardPopup, cardPopup,
   imageAddFormElement, placesContainer, cardTitleInput, cardLinkInput, config, imagePopup
-} from '../utils/constants.js';
+} from './utils/constants.js';
 
 
-import Section from '../components/Section.js';
-import PopupWithImage from "../components/PopupWithImage.js";
-import PopupWithForm from "../components/PopupWithForm.js";
-import UserInfo from "../components/UserInfo.js";
+import Section from './components/Section.js';
+import PopupWithImage from "./components/PopupWithImage.js";
+import PopupWithForm from "./components/PopupWithForm.js";
+import UserInfo from "./components/UserInfo.js";
 
 const userInfo = new UserInfo(profileName, profileJob)
 
